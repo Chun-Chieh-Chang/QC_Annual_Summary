44e39ec561baf1ed92efcab7b3028e358804db9f
chun-chieh-chang <wesleychang2025@gmail.com>
2026-07-14 21:17:04 +0800

    chore: rebrand project to QC_Annual_Summary with updated base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FileName_WorkSheet_Extract/',
+  base: '/QC_Annual_Summary/',
 })
